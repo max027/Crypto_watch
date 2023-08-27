38976942384e3735f4d2d03db27eb955bc9c788c
saurabh wajage <saurabhwajage887@gmail.com>
2023-08-27 19:19:30 +0530

    added firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'crypto-watch-3e845',
+    appId: '1:526566492324:web:38906b78351581be8c3e31',
+    storageBucket: 'crypto-watch-3e845.appspot.com',
+    apiKey: 'AIzaSyBtf14OQfbgu0Nk2GXF64qkMYIs7EBthjA',
+    authDomain: 'crypto-watch-3e845.firebaseapp.com',
+    messagingSenderId: '526566492324',
+    measurementId: 'G-HYWNGZD0DP',
+  },
+};
